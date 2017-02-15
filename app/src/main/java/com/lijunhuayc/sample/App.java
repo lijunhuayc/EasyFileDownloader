1742ab602f3e208c20bd9e93d83f223ebbe08d15
jli <jli@bpexps.com>
2017-02-15 18:13:40 +0800

    移出未使用的NotificationHelper类

--- app/src/main/java/com/lijunhuayc/sample/App.java
+++ app/src/main/java/com/lijunhuayc/sample/App.java
@@ -2,8 +2,6 @@ package com.lijunhuayc.sample;
 
 import android.app.Application;
 
-import com.lijunhuayc.downloader.notification.NotificationHelper;
-
 /**
  * Desc:
  * Created by ${junhua.li} on 2016/09/28 09:33.
@@ -14,6 +12,5 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        NotificationHelper.init(this);
     }
 }
